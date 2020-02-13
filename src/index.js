16a1e62b1d4cf24970c1909b071ce296323ad835
trox115 <tofernandes67@gmail.com>
2020-02-13 15:25:04 +0000

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { createStore } from 'redux';
 import App from './App';
 import allReducers from './reducers';
 
-const store = createStore(allReducers);
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<App />, document.getElementById('root'));
