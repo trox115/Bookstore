d410ac49a2a2c33026e5a2768a6689d96b4e1e9a
trox115 <tofernandes67@gmail.com>
2020-02-17 21:32:34 +0000

    fixed case/default clauses

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -5,10 +5,10 @@ const books = (state = null, action) => {
   switch (action.type) {
     case 'CREATE_BOOK':
       return [...state, action.book];
-    case 'REMOVE_BOOK':
-      let index = -1;
-      index = state.findIndex(action.book);
+    case 'REMOVE_BOOK':{
+      const index = state.findIndex(action.book);
       return arrayRemove(state, index);
+      }
     default:
       return state;
   }
