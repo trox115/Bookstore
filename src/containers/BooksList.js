ec2039d743caa290088a2a5d6a6f8f0956eed904
trox115 <tofernandes67@gmail.com>
2020-02-18 21:58:01 +0000

    BookList Component should be written as a pure function corrected

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,24 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
 
-class BooksList extends React.Component {
-  render() {
-    console.log(this.props.books);
-    const books = this.props.books.map(book => <Book id={book.id} book={book} />);
-    return (
-      <div>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Category</th>
-          </tr>
-          {books}
-        </table>
-      </div>
-    );
-  }
-}
+const BooksList = ({ books }) => {
+  const library = books.map(book => <Book id={book.id} book={book} />);
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Category</th>
+        </tr>
+        {library}
+      </table>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   books: state.books,
