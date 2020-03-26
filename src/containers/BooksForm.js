53a887e3a3ac3b88c2da5a6f73684d27af7c787d
trox115 <tofernandes67@gmail.com>
2020-03-26 18:11:13 +0000

    Changed function to class, handleChange implemented!

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,27 +1,65 @@
 import React from 'react';
 
-const BookForm = () => {
-  const category = ['Action', 'Biography', 'Horror', 'History', 'Kids', 'Learning', 'Sci-fi'];
-  return (
-    <form>
-      <label htmlFor="title">
-        Title
-        <input type="text" name="title" id="title" />
-        <br />
-      </label>
-      <label htmlFor="category">
-        Category
-        <select id="category" required>
-          {category.map(category => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
+function randomNumber() {
+  return Math.floor(Math.random() * 101);
+}
+
+const initialState = {
+  title: '',
+  category: 'Action',
+  id: randomNumber(),
 };
+
+class BookForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('the movie title is ', this.state.title);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    const { title } = this.state;
+    const category = ['Action', 'Biography', 'Horror', 'History', 'Kids', 'Learning', 'Sci-fi'];
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="title">
+          Title
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <p> The title is :{title} </p>
+          <br />
+        </label>
+        <label htmlFor="category">
+          Category
+          <select id="category" required>
+            {category.map(category => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
 
 export default BookForm;
